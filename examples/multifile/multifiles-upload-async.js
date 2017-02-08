e578a0412c00b00a6df8011e2d9d2998cb38dfc0
ElijahKR <2063390754@qq.com>
2017-02-08 23:48:05 +0800

    Add files via upload

---
+++ examples/multifile/multifiles-upload-async.js
@@ -0,0 +1,180 @@
+﻿var fileInfo = function () {
+    return {
+        index: '',
+        tag: {},
+        upload: [],
+        total: ''
+    };
+}
+
+var multiupload = function (args) {
+    var current = $(this);
+
+    var uniqueNo = 0;
+    var uploads = [];
+    var fileTagTemplate = '<input type="file" multiple="multiple" style="display:none;" />';
+
+    /*intefaces*/
+    current.upload = function () {
+        //append to stack
+        var currFileInfo = new fileInfo();
+        currFileInfo.index = uniqueNo++;
+
+        //append file tag
+        var currFileTag = $(fileTagTemplate);
+        currFileTag.attr('unique', currFileInfo.index);
+        $('body').append(currFileTag);
+
+        currFileInfo.tag = currFileTag;
+        uploads.push(currFileInfo);
+
+        //initialize file event
+        currFileTag.change(function () {
+            var files = currFileTag.get(0).files;
+
+            var currIndex = 0;
+            var filecount = files.length;
+            currFileInfo.total = filecount;
+
+            (function execute() {
+                initupload(currFileInfo.index, files[currIndex++]);
+                setTimeout(function () {
+                    if (currIndex < filecount) {
+                        execute();
+                    }
+                }, 100);
+            })();
+        });
+
+        currFileTag.click();
+    }
+    current.cancel = function (index, key) {
+        loop(uploads, function (up) {
+            loop(up.upload, function (currUpload) {
+                if (currUpload.key == key) {
+                    currUpload.cancel();
+                }
+            });
+        });
+    }
+    current.pause = function (index, key) {
+        loop(uploads, function (up) {
+            loop(up.upload, function (currUpload) {
+                if (currUpload.key == key) {
+                    currUpload.pause();
+                }
+            });
+        });
+    }
+    current.continue = function (index, key) {
+        loop(uploads, function (up) {
+            loop(up.upload, function (currUpload) {
+                if (currUpload.key == key) {
+                    currUpload.continue();
+                }
+            });
+        });
+    }
+    current.dispose = function () {
+        loop(uploads, function (up) {
+            loop(up.upload, function (currUpload) {
+                currUpload.cancel();
+            });
+            up.tag.remove();
+        });
+    }
+
+    /*auxiliary*/
+    function initupload(index, file) {
+        var currUpload = new upload({
+            url: args.url,
+            file: file
+        });
+        currUpload.key = Math.random();
+
+        //append
+        appendUpload(index, currUpload);
+
+        //events
+        currUpload.start(function () {
+            execute(args.start, { index: index, key: currUpload.key, name: file.name, upload: currUpload });
+        });
+
+        currUpload.cancel(function () {
+            execute(args.cancel, { index: index, key: currUpload.key, name: file.name, upload: currUpload });
+        });
+
+        currUpload.process(function (ev, data) {
+            execute(args.process, { index: index, key: currUpload.key, name: file.name, upload: currUpload, data: data });
+        });
+
+        currUpload.success(function () {
+            execute(args.success, { index: index, key: currUpload.key, name: file.name, upload: currUpload });
+        });
+
+        currUpload.error(function (ev, data) {
+            execute(args.error, { index: index, key: currUpload.key, name: file.name, upload: currUpload, msg: data.msg });
+        });
+
+        currUpload.complete(function () {
+            removeUpload(index);
+            execute(args.complete, { index: index, key: currUpload.key, name: file.name, upload: currUpload });
+        });
+
+        currUpload.pause(function () {
+            execute(args.pause, { index: index, key: currUpload.key, name: file.name, upload: currUpload });
+        });
+
+        currUpload.continue(function () {
+            execute(args.contine, { index: index, key: currUpload.key, name: file.name, upload: currUpload });
+        });
+
+        //start
+        currUpload.start();
+    }
+
+    function findUpload(index) {
+        var currUp = undefined;
+        loop(uploads, function (up) {
+            if (up.index == index) {
+                currUp = up;
+            }
+        });
+        return currUp;
+    }
+
+    function appendUpload(index, upload) {
+        loop(uploads, function (up) {
+            if (up.index == index) {
+                up.upload.push(upload);
+            }
+        });
+    }
+
+    function removeUpload(index) {
+        var up = findUpload(index);
+        if (up) {
+            up.total--;
+            if (up.total <= 0) {
+                if (up.tag.remove) {
+                    up.tag.remove();
+                }
+            }
+        }
+    }
+
+    function execute(callback, params) {
+        if (callback && typeof (callback) == 'function') {
+            callback(params);
+        }
+    }
+
+    function loop(array, callback) {
+        for (var i = 0; i < array.length; i++) {
+            var curr = array[i];
+            execute(callback, curr);
+        }
+    }
+
+    return current;
+}
---
+++ examples/multifile/upload-async.js
@@ -0,0 +1,165 @@
+﻿/*variables*/
+var eventsDefination = {
+    start: 'start',
+    cancel: 'cancel',
+    process: 'process',
+    success: 'success',
+    error: 'error',
+    complete: 'complete',
+    pause: 'pause',
+    continue: 'continue'
+}
+
+var upload = function (args) {
+    var current = $(this);
+    var processing = true;
+    var pausing = false;
+    var cacheInfo = {
+        file: [],
+        obj: []
+    };
+
+    /*chunk size*/
+    current.chunksize = 512 * 1024;
+    if (args.chunksize) {
+        chunksize = args.chunksize;
+    }
+
+    /*events*/
+    current.start = function (callback) {
+        eventsBind(eventsDefination.start, callback, function () {
+            processing = true;
+            if (args.file) {
+                uploadasync(args.file, 0);
+            }
+        });
+    }
+    current.cancel = function (callback) {
+        eventsBind(eventsDefination.cancel, callback, function () {
+            processing = false;
+        });
+    }
+    current.process = function (callback) {
+        eventsBind(eventsDefination.process, callback);
+    }
+    current.success = function (callback) {
+        eventsBind(eventsDefination.success, callback);
+    }
+    current.error = function (callback) {
+        eventsBind(eventsDefination.error, callback);
+    }
+    current.complete = function (callback) {
+        eventsBind(eventsDefination.complete, callback);
+    }
+    current.pause = function (callback) {
+        eventsBind(eventsDefination.pause, callback, function () {
+            pausing = true;
+        });
+    }
+    current.continue = function (callback) {
+        eventsBind(eventsDefination.continue, callback, function () {
+            if (pausing) {
+                pausing = false;
+                if (cacheInfo.file && cacheInfo.obj) {
+                    uploadasync(cacheInfo.file, cacheInfo.obj.start, cacheInfo.obj.name);
+                }
+            }
+        });
+    }
+
+    /*auxiliary methods*/
+    function uploadasync(file, start, name) {
+        var totlelength = file.size;
+
+        var percent = 100 * start / totlelength;
+        current.trigger(eventsDefination.process, { percent: percent.toFixed(2), name: file.name });
+
+        if (start < totlelength) {
+            var formdata = new FormData();
+            var currChunkSize = current.chunksize;
+            var end = start;
+
+            if (start == 0) {
+                name = file.name;
+            }
+
+            if (start < file.size) {
+                if (currChunkSize > totlelength) {
+                    currChunkSize = totlelength;
+                }
+                end = start + currChunkSize;
+                if (end >= totlelength) {
+                    end = totlelength
+                }
+
+                formdata.append('file', file.slice(start, end));
+                formdata.append('total', totlelength);
+                formdata.append('name', name);
+                formdata.append('filename', file.name);
+                formdata.append('start', start);
+                formdata.append('end', end);
+            }
+
+            $.ajax({
+                url: args.url,
+                type: 'post',
+                dataType: 'json',
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: formdata,
+                error: function (response, status, error) {
+                    current.trigger(eventsDefination.error, { percent: percent.toFixed(2), name: file.name, msg: error });
+                    current.trigger(eventsDefination.complete);
+                },
+                success: function (data) {
+                    if (data.flag) {
+                        if (processing) {
+                            if (!pausing) {
+                                uploadasync(file, data.obj.start, data.obj.name);
+                            }
+                            else {
+                                cacheInfo.file = file;
+                                cacheInfo.obj = data.obj;
+                            }
+                        }
+                        else {
+                            current.trigger(eventsDefination.cancel);
+                            current.trigger(eventsDefination.complete);
+                        }
+                    }
+                    else {
+                        current.trigger(eventsDefination.error, { percent: percent.toFixed(2), name: file.name, msg: data.msg });
+                        current.trigger(eventsDefination.complete);
+                    }
+                },
+                complete: function () {
+                    
+                }
+            });
+        }
+        else {
+            cacheInfo = { file: [], obj: [] };
+            current.trigger(eventsDefination.success);
+            current.trigger(eventsDefination.complete);
+        }
+    }
+
+    function execute(callback, params) {
+        if (callback && typeof (callback) == 'function') {
+            callback(params);
+        }
+    }
+
+    function eventsBind(eventName, callback, triggerExecution) {
+        if (callback && typeof (callback) == 'function') {
+            current.on(eventName, callback);
+        }
+        else {
+            current.trigger(eventName)
+            execute(triggerExecution);
+        }
+    };
+
+    return current;
+}
